7613c868ca2fff0bd9ae8b235d4444be433c4c9e
nzenEng <55366408+nyarbasi25@users.noreply.github.com>
2021-11-11 10:24:54 -0500

    Add files via upload

---
+++ PythonKnowledge/squareNumber.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 20:53:15 2021
+
+@author: nyarbasi
+"""
+
+"""
+Spyder Editor
+Square a number 
+
+"""
+
+# input a number
+digit = int(input("Enter an integer number: "))
+
+# calculate square using exponent operator
+square = digit**2
+
+# print
+print(f"Square of {digit} is {square}")
